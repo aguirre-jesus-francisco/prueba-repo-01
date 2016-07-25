eb1bbeb4e68f4a05be49592e4a4e95ef45d6dda0
jesus <jesus@jesus-Satellite-L845>
2016-07-25 17:12:33 -0300

    6

---
+++ prueba_2/src/prueba_2.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : prueba_2.c
+ Author      : jesus_2
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hola Nicolas!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
